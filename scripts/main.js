3e754093a6ee2a2e2a209e36803ead477cabd778
William <kebekad673@proton.me>
2024-02-24 05:46:44 -0600

    Initalized boilerplate filestructure

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+// const { <objectName> , <functionName> } = require("scripts/data.js");
+// 
+// const { <objectName> , <functionName> } = require("scripts/functions.js");
+// 
+// Initialize an empty object
+let myObject = {};
+
+let myArray = [];
+
+// Declare three functions
+const function1 = () => {
+  console.log("Function 1 called");
+}
+
+const function2 = () => {
+  console.log("Function 2 called");
+}
+
+const function3 = () => {
+  console.log("Function 3 called");
+}
+
+
+// Example: Call the functions
+myObject.function1();
+myObject.function2();
+myObject.function3();
